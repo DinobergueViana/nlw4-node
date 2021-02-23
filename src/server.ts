2abdf169d8b896b6f9885c64cd96f44f02343cc7
DinobergueViana <dinobergueviana@gmail.com>
2021-02-23 13:25:51 -0300

    configurando conexao com o banco

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,12 @@
+import 'reflect-metadata';
 import express from 'express';
+import "./database";
+import { router } from './routes'
 
 const app = express();
 
-// definindo as primeiras rotas de acesso
-app.get("/", (req, res) => {
-  return res.status(200).json({msg: "Hello World! NLW4 Node!!"})
-});
-
-app.post("/", (req, res) => {
-  return res.status(201).json({msg: "User created"})
-});
+app.use(express.json())
+app.use(router)
 
 // levantando servidor
 app.listen(3000, () => console.log("Server is running!"));
